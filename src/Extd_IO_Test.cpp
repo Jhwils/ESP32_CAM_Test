f6142432930608978ac527f17ffefc4fffbe630f
Jhwils <hujunjian89@outlook.com>
2025-07-03 23:20:13 +0800

    Second save: Initialize the I2C communication, add some function to realize the reading and writing bits to PCA9534 registers

--- src/Extd_IO_Test.cpp
+++ src/Extd_IO_Test.cpp
@@ -4,10 +4,43 @@
 
 //根据芯片原理图，A0=0, A1=0, A2=1;
 #define PCA9534_ADDR 0x24
+#define REG_INPUT 0x00 // 输入寄存器
+#define REG_OUTPUT 0x01 // 输出寄存器   
+#define REG_POLARITY 0x02 // 极性寄存器
+#define REG_CONFIG 0x03 // 配置寄存器
 
 // 引脚定义 (ESP32)
 const int sdaPin = 41;
 const int sclPin = 42;
+void PCA_Write(uint8_t reg, uint8_t data);
+
+
+void RTC_SCL_init() 
+{
+	Wire.begin(sdaPin, sclPin); // 初始化I2C，指定SDA和SCL引脚
+    PCA_Write(REG_CONFIG, 0xF0); //设置P0~P4为输出，P5~P7为输入
+}
+
+//写入PCA9534寄存器
+void PCA_Write(uint8_t reg, uint8_t data) {
+    Wire.beginTransmission(PCA9534_ADDR); //写入PCA9534地址，开始I2C通信
+    Wire.write(reg); // 写入寄存器地址
+    Wire.write(data); // 写入数据
+    Wire.endTransmission(); // 结束I2C通信
+    if (Wire.endTransmission() != 0) {
+        Serial.println("Error: Write failed");
+    }    
+
+}
+
+// 读取PCA9534寄存器
+uint8_t PCA_Read(uint8_t reg) {
+  Wire.beginTransmission(PCA9534_ADDR);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(PCA9534_ADDR, 1);
+  return Wire.read();
+}
 
 
 
